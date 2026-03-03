4dd514499f8f47d6b178cf49e58491eb7f30fcfe
Jaci26cx <Ja824325@ucf.edu>
2026-03-02 23:12:30 -0500

    added the list of exercises and updated css

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Select from the list of Exercises:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+        <li><button>Push Ups</button></li>
+        <li><button>Running</button></li>
+      </ul>
+      
       </header>
     </div>
   );
