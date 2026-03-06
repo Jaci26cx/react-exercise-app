dfb31ec083307b5da6d5eb3a31fea996ecad0bbf
Jaci26cx <Ja824325@ucf.edu>
2026-03-05 22:58:28 -0500

    added components & modify some things

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 
-function App() {
+function App () {
+
+//Didn't do lastweeks assignment but wanted to learn how to export something onto gh pages
   return (
     <div className="App">
-      <header className="App-header">
+  
         <p>
           Select from the list of Exercises:
         </p>
         <ul>
         <li><button>Push Ups</button></li>
         <li><button>Running</button></li>
+        <li><button>Planks</button></li>
       </ul>
-      
-      </header>
     </div>
   );
 }
